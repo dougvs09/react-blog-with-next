92ad7d0beebf71b54fa1ed7cbcc98cf81e99d4da
Douglas Valente <douglasvalente6@gmail.com>
2022-01-11 20:24:24 -0300

    feat
    add the posts in the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,61 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 
 import Header from '../components/Header'
+import PostsCards from '../components/PostsCards'
+import { Main, PostsWrapper } from '../styles/HomeStyle'
+import getAllPosts from '../utils/getAllPosts'
 
-const Home: NextPage = () => {
-  return <Header></Header>
+type PostType = {
+  _id: string
+  content: string
+  title: string
+  author: {
+    id: string
+    name: string
+    avatar: string
+  }
+  image: string
+  category: string
+  createdAt: string
+  isHighlighted: boolean
+}
+
+interface AllPostsTypes {
+  allPostsData: PostType[]
+}
+
+const Home: NextPage<AllPostsTypes> = ({ allPostsData }: AllPostsTypes) => {
+  return (
+    <>
+      <Header />
+      <Main>
+        <PostsWrapper>
+          {allPostsData.map((data) => (
+            <PostsCards
+              key={data._id}
+              path={data._id}
+              postPicture={data.image}
+              category={data.category}
+              title={data.title}
+              createdAt={data.createdAt}
+              isHighlighted={data.isHighlighted}
+            />
+          ))}
+        </PostsWrapper>
+      </Main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = await getAllPosts()
+
+  return {
+    props: {
+      allPostsData,
+    },
+    revalidate: 10,
+  }
 }
 
 export default Home
---
+++ src/styles/HomeStyle.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Main = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+export const PostsWrapper = styled.div`
+  max-width: 1200px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 40px 20px;
+  margin: 0 auto;
+`
