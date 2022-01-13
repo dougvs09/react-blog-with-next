6e4bf6b5b17028c23ccda9ec4616df73239430d6
Douglas Valente <douglasvalente6@gmail.com>
2022-01-12 23:21:13 -0300

    feat
    add SessionProvider for auth and add progress bar for loading pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,32 @@
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import Router from 'next/router'
+
+import '../styles/nprogress.css'
+import NProgress from 'nprogress'
 
 import SwitchThemeContextProvider from '../contexts/SwitchThemeContext'
 import GlobalStyles from '../styles/GlobalStyles'
 
-function MyApp({ Component, pageProps }: AppProps) {
+Router.events.on('routeChangeStart', () => {
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done()
+})
+Router.events.on('routeChangeError', () => {
+  NProgress.done()
+})
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
-      <SwitchThemeContextProvider>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </SwitchThemeContextProvider>
+      <SessionProvider session={session}>
+        <SwitchThemeContextProvider>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </SwitchThemeContextProvider>
+      </SessionProvider>
     </>
   )
 }
