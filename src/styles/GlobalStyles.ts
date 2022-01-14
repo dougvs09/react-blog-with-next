de8e65aaa44cb0923bf3a55f1679189ce0683242
Douglas Valente <douglasvalente6@gmail.com>
2022-01-14 18:55:01 -0300

    refactor

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -7,10 +7,11 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  html, body, #root {
+  html, body, #__next {
     background: ${(props) => props.theme.colors.background};
     color: ${(props) => props.theme.colors.fontColor};
     font: normal 24px 'Source Sans Pro', sans-serif;
+    height: 100%;
   }
 
   ul, li, ol {
