e1d61aa3615c45a58f14a764d4cbb82bd26eaf67
Douglas Valente <douglasvalente6@gmail.com>
2022-01-14 20:00:29 -0300

    refactor

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,15 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID || ''
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || ''
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
     }),
   ],
   secret: 'secret token',
