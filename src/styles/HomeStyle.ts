1b02c8f4785db8275aeb48cbf7accc8e88bc3f32
Douglas Valente <douglasvalente6@gmail.com>
2022-01-14 11:53:25 -0300

    feat
    add separator in homepage

--- src/styles/HomeStyle.ts
+++ src/styles/HomeStyle.ts
@@ -14,3 +14,26 @@ export const PostsWrapper = styled.div`
   padding: 40px 20px;
   margin: 0 auto;
 `
+
+export const Separator = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+  padding: 20px 20px 10px 20px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+
+  &:after,
+  &:before {
+    content: '';
+    width: 43.4%;
+    height: 2px;
+    background: ${({ theme }) => theme.colors.gray};
+    display: block;
+  }
+
+  p {
+    font: 600 14px 'Source Sans Pro', sans-serif;
+    color: ${({ theme }) => theme.colors.gray};
+  }
+`
